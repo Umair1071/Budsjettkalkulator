cd5f58b5d866dbffdc8702509db7cea904578ac0
Umair1071 <umira002@osloskolen.no>
2026-09-24 22:22:54 +0200

    Ferdig med budsjettkalkulatoren
    Jeg er ferdig med budsjettkalkulatoren. Jeg har fått på plass inntekter, utgifter, beregning av saldo og designet. Nå er prosjektet klart for levering.

--- script.js
+++ script.js
@@ -1,1 +1,79 @@
+let inntekter = []
+let utgifter = []
 
+const inntektKnapp = document.getElementById("leggTilInntekt")
+const utgiftKnapp = document.getElementById("leggTilUtgift")
+
+inntektKnapp.addEventListener("click", function() {
+
+    const navn = document.getElementById("inntektNavn").value
+    const belop = Number(document.getElementById("inntektBelop").value)
+
+    if (navn == "" || belop <= 0) {
+        return
+    }
+
+    inntekter.push({
+        navn: navn,
+        belop: belop
+    })
+
+    document.getElementById("inntekter").innerHTML +=
+        "<p>" + navn + ": " + belop + " kr</p>"
+
+    document.getElementById("inntektNavn").value = ""
+    document.getElementById("inntektBelop").value = ""
+
+    beregnBudsjett()
+})
+
+
+utgiftKnapp.addEventListener("click", function() {
+
+    const navn = document.getElementById("utgiftNavn").value
+    const belop = Number(document.getElementById("utgiftBelop").value)
+
+    if (navn == "" || belop <= 0) {
+        return
+    }
+
+    utgifter.push({
+        navn: navn,
+        belop: belop
+    })
+
+    document.getElementById("utgifter").innerHTML +=
+        "<p>" + navn + ": " + belop + " kr</p>"
+
+    document.getElementById("utgiftNavn").value = ""
+    document.getElementById("utgiftBelop").value = ""
+
+    beregnBudsjett()
+})
+
+
+function beregnBudsjett() {
+
+    let totalInntekt = 0
+    let totalUtgift = 0
+
+    for (let inntekt of inntekter) {
+        totalInntekt = totalInntekt + inntekt.belop
+    }
+
+    for (let utgift of utgifter) {
+        totalUtgift = totalUtgift + utgift.belop
+    }
+
+    let saldo = totalInntekt - totalUtgift
+
+    document.getElementById("totalInntekt").textContent = totalInntekt
+    document.getElementById("totalUtgift").textContent = totalUtgift
+    document.getElementById("saldo").textContent = saldo
+
+    if (saldo < 0) {
+        document.getElementById("saldo").style.color = "red"
+    } else {
+        document.getElementById("saldo").style.color = "green"
+    }
+}
